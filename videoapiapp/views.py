751274f95a91c4b177efe65d545caf2eddc3f8f7
Koech-code <nixonkipkorir01@gmail.com>
2022-09-24 08:25:30 +0300

    create view to post and get videos, as well as comment on the specific video post

--- videoapiapp/views.py
+++ videoapiapp/views.py
@@ -1,25 +1,48 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import parser_classes
-from rest_framework.parsers import FileUploadParser
 from rest_framework.views import APIView
 from rest_framework import status
 from .models import video, comments
-from .serializer import VideoSerializer
+from .serializer import VideoSerializer, CommentSerializer
+from rest_framework.parsers import MultiPartParser, FormParser
 
 # Create your views here.
 class allvideos(APIView):
-    parser_classes = [FileUploadParser]
+    parse_classes = (MultiPartParser, FormParser)
     def get(self, request):
         postedvideos = video.objects.all()
         serializer = VideoSerializer(postedvideos, many=True)
 
         return Response(serializer.data)
 
-    def post(self, request, format=None):
-        video_file = request.FILES['file']
-        destination = open('/Users/Username/' + video_file.name, 'wb+')
-        for chunk in video_file.chunks():
-            destination.write(chunk)
-        destination.close()  
-        return Response(video_file.name, status.HTTP_201_CREATED)
+    def post(self, request, *args, **kwargs):
+        
+        serializers = VideoSerializer(data=request.data)
+ 
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data,
+            status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializers.errors, 
+            status=status.HTTP_400_BAD_REQUEST)
+
+class commentvideos(APIView):
+    def get(self, request):
+        commentedvideos = comments.objects.all()
+        serializer = CommentSerializer(commentedvideos, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        
+        comment_serializers = CommentSerializer(data=request.data)
+ 
+        if comment_serializers.is_valid():
+            comment_serializers.save()
+            return Response(comment_serializers.data,
+            status=status.HTTP_201_CREATED)
+        else:
+            return Response(comment_serializers.errors, 
+            status=status.HTTP_400_BAD_REQUEST)
+
