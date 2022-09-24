c67ca24a507f6794e2df50fc556d4e22d423b515
Koech-code <nixonkipkorir01@gmail.com>
2022-09-24 08:23:58 +0300

    create serializer to format data to JSON

--- videoapiapp/serializer.py
+++ videoapiapp/serializer.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
-from rest_framework.serializers import Serializer, FileField
 from .models import video, comments
 
 class VideoSerializer(serializers.ModelSerializer):
-    video = FileField()
     class Meta:
         model = video
         fields = ('name', 'video', 'description')
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = comments
+        fields = ('visitorspost', 'visitorscomment')
